89dc34b299f09b20e9b3fcff20949c2185d56e49
admin <lmhtzedd@gmail.com>
2023-11-28 11:24:13 +0700

    commit

---
+++ app/src/main/java/com/example/shugamerstore/Adapters/GameListAdapter.java
@@ -0,0 +1,72 @@
+package com.example.shugamerstore.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.PixelCopy;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.shugamerstore.Domain.ListGame;
+import com.example.shugamerstore.R;
+
+public class GameListAdapter extends RecyclerView.Adapter<GameListAdapter.ViewHolder> {
+    ListGame items;
+    Context context;
+
+    public GameListAdapter(ListGame items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public GameListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context=parent.getContext();
+        View inflate= LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_game,parent,false);
+        return new ViewHolder(inflate);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GameListAdapter.ViewHolder holder, int position) {
+        holder.titleTxt.setText(items.getData().get(position).getTitle());
+        RequestOptions requestOptions=new RequestOptions();
+        requestOptions=requestOptions.transform(new CenterCrop(),new RoundedCorners(30));
+
+        Glide.with(context)
+                .load(items.getData().get(position).getPoster())
+                .apply(requestOptions)
+                .into(holder.pic);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return items.getData().size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView titleTxt;
+        ImageView pic;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTxt=itemView.findViewById(R.id.titleTxT);
+            pic=itemView.findViewById(R.id.pic);
+        }
+    }
+}
