0696f582c2032566c9a1a46d981ee96dd7f2be11
admin <lmhtzedd@gmail.com>
2023-12-20 01:26:41 +0700

    commit

--- app/src/main/java/com/example/shugamerstore/Adapters/GameListAdapter.java
+++ app/src/main/java/com/example/shugamerstore/Adapters/GameListAdapter.java
@@ -1,9 +1,5 @@
 package com.example.shugamerstore.Adapters;
 
-import android.content.Context;
-import android.content.Intent;
-import android.view.LayoutInflater;
-import android.view.PixelCopy;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,54 +8,22 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.CenterCrop;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
-import com.bumptech.glide.request.RequestOptions;
-import com.example.shugamerstore.Activities.DetailActivity;
-import com.example.shugamerstore.Domain.ListFilm;
-import com.example.shugamerstore.Domain.ListGame;
-import com.example.shugamerstore.R;
-
 public class GameListAdapter extends RecyclerView.Adapter<GameListAdapter.ViewHolder> {
-    ListFilm items;
-    Context context;
-
-    public GameListAdapter(ListGame items) {
-        this.items = items;
-    }
-
+    G
     @NonNull
     @Override
     public GameListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        context=parent.getContext();
-        View inflate= LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_game,parent,false);
-        return new ViewHolder(inflate);
         return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull GameListAdapter.ViewHolder holder, int position) {
-        holder.titleTxt.setText(items.getData().get(position).getTitle());
-        RequestOptions requestOptions=new RequestOptions();
-        requestOptions=requestOptions.transform(new CenterCrop(),new RoundedCorners(30));
 
-        Glide.with(context)
-                .load(items.getData().get(position).getPoster())
-                .apply(requestOptions)
-                .into(holder.pic);
-
-        holder.itemView.setOnClickListener(v -> {
-            Intent intent=new Intent(holder.itemView.getContext(), DetailActivity.class);
-            intent.putExtra("id", items.getData().get(position).getId());
-            context.startActivity(intent);
-        });
     }
 
     @Override
     public int getItemCount() {
-
-        return items.getData().size();
+        return 0;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
@@ -67,8 +31,8 @@ public class GameListAdapter extends RecyclerView.Adapter<GameListAdapter.ViewHo
         ImageView pic;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            titleTxt=itemView.findViewById(R.id.titleTxT);
-            pic=itemView.findViewById(R.id.pic);
+            titleTxt=itemView.findViewById(R.id.)
         }
+
     }
 }
